1f126d50d0fcb9c88607b8a2c7bd6e9fb4d272a7
Ben Schenerman <ben.schenerman@gmail.com>
2019-10-28 09:33:32 -0400

    🐛 Add port listening on local environment variable

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-    // console.log('a user connected');
-    console.log(socket)
+    console.log('a user connected');
+    // console.log(socket)
     socket.on('chat message', function (msg) {
+        console.log('user msg: '+msg);
         io.emit('chat message', msg);
     });
 
-    // socket.on('disconnect', function () {
-    //     console.log('user disconnected');
-    // });
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(port, function () {
+    console.log(`listening on /:${port}`);
 });

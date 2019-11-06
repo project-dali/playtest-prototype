4a7319eb789f9ff006b23b1887d4448ba0cd531a
Beck Rivera <ber46@drexel.edu>
2019-11-06 01:47:16 -0500

    ✨Add output to Data.JSON for all sent info
    ✨Add output for New Rounds + All User Responses to data.json
    ✨Add 'new game' Socket Emit
    💄Update 'new round' so it uses prompts we made for playtesting
    💄Update 'new round' so it runs through each prompt once per game (no repeat prompts)

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var app = require('express')();
+const fs = require('fs');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var prompts = ['Schneef', 'Grass', 'Pumpkins are classified as a fruit.', 'Tart', 'TFOML', 'If there\'s a fire, I...', 'Plant Mom', 'Scurvy Dog', 'Trash Panda', 'Bold Move', 'Cereal is soup.', 'How to really keep the doctor away', 'Bad Traffic Law', 'Tuesday Night', 'Purple Thumb', 'Google Driving', 'Super Secret Storage', 'Santa', 'USPS', 'Trash Day', 'Stop Signs', 'Church of ___', 'Drexel\'s School for the ___'];
+var usedPrompts = [];
+var round = 1;
+var rndResponses = 0;
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -13,19 +18,65 @@ app.get('/host', function (req, res) {
 
 io.on('connection', function (socket) {
     console.log('a user connected');
-    // console.log(socket)
     socket.on('chat message', function (msg) {
-        console.log('user msg: '+msg);
+        rndResponses++;
+        let msgData = { 
+            response_num: rndResponses,
+            message: msg
+        };
+        let data = JSON.stringify(msgData, null, 2);
+         try {
+            fs.appendFileSync('data.json', data);
+            console.log('The User Response was appended to the file!');
+          } catch (err) {
+            throw err;
+          }
+
+        console.log('user msg: ' + msg);
         io.emit('chat message', msg);
     });
 
+    socket.on('new game', function(){
+        round = 1;
+        usedPrompts = [];
+        rndResponses = 0;
+        console.log('New game started!');
+    })
+
     socket.on('new round', function(){
-        let prompt = 'Boiiiii';
-        let responseMsg = 'timestamp; prompt_id=00; prompt: ' + prompt;
+        rndResponses = 0;
+        if(usedPrompts.length == 23) {
+            round = 1;
+            usedPrompts = [];
+            console.log('New game started!');
+        }
+        let i = Math.floor(Math.random() * prompts.length);
+        while(usedPrompts.includes(i)) {
+            i = Math.floor(Math.random() * prompts.length);
+        }
+        usedPrompts.push(i);
+        let responseMsg = 'Round ' + round + '; prompt_id=' + i + '; prompt: ' + prompts[i];
+
+        let roundData = { 
+            round: round,
+            prompt_id: i, 
+            prompt: prompts[i], 
+        };
+
+        let data = JSON.stringify(roundData, null, 2);
+
+        try {
+            fs.appendFileSync('data.json', data);
+            console.log('The User Response was appended to the file!');
+          } catch (err) {
+            throw err;
+          }
         
         console.log(responseMsg)
         io.emit('chat message', responseMsg);
-    })
+
+        round++;
+    });
 
     socket.on('send survey', function(){
         let responseMsg = 'survey mode initiated';
@@ -33,7 +84,7 @@ io.on('connection', function (socket) {
         
         io.emit('chat message', responseMsg)
         io.emit('take survey');
-    })
+    });
 
     socket.on('disconnect', function () {
         console.log('user disconnected');
